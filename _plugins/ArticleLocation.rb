8d6d883434817670c0deb7639fb126f90dedd812
julianblane <jbenegaslane@gmail.com>
2025-01-23 23:04:22 -0300

    crop map, account for cropping in pin location

--- _plugins/ArticleLocation.rb
+++ _plugins/ArticleLocation.rb
@@ -2,30 +2,37 @@ require 'uri'
 
 module Jekyll
     class ArticleLocation < Generator
-      # generate perent x and y values for each article based on latitude and longitude
+      # generate fraction x and y values for each article based on latitude and longitude
       def generate(site)
         site.data['articles'].each do |article|
           host = URI.parse(article["url"]).host
           article["source"] = host.nil? ? '' : host.sub(/^www\./, '')
 
-          next unless article["longitude"] && article["latitude"]
+        next unless article["longitude"] && article["latitude"]
 
-          map_width = 100
-          map_height = 100
-          
-          # get x value
-          x = (article["longitude"] + 180) * (map_width / 360.0)
-          
-          # convert from degrees to radians
-          lat_rad = article["latitude"] * Math::PI / 180
-          
-          # get y value
-          merc_n = Math.log(Math.tan((Math::PI / 4) + (lat_rad / 2)))
-          y = (map_height / 2) - (map_width * merc_n / (2 * Math::PI))
+        map_top_cropping = 14.5
+        map_bottom_cropping = 29.01
 
-          article["x"] = x
-          article["y"] = y
-        end
+        map_width = 100
+        map_height = 100
+        
+        # get x value
+        x = (article["longitude"] + 180) * (map_width / 360.0)
+        
+        # convert from degrees to radians
+        lat_rad = article["latitude"] * Math::PI / 180
+        
+        # get y value
+        merc_n = Math.log(Math.tan((Math::PI / 4) + (lat_rad / 2)))
+        y = (map_height / 2) - (map_width * merc_n / (2 * Math::PI))
+
+        article["x"] = x
+
+        y = ((y / 100) - (map_top_cropping / map_height)) / (1 - (map_top_cropping / map_height)) * 100
+        y = ((y / 100) / (1 - (map_bottom_cropping / (map_height - map_top_cropping)))) * 100
+
+        article["y"] = y
       end
     end
+  end
   end
